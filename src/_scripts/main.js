0c7e6c58618e2856887225ee5195defd0497414b
ia <ayiltek@gmail.com>
2017-06-11 23:15:16 +0300

    with jsonplaceholder

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -5,12 +5,12 @@ import 'angular';
 var aTodo=angular.module("todoApp",[]);
 aTodo.controller("todocontrol",function($scope,$http){
 
-    $http.get("data/data.json").then(function(response){
+    $http.get("https://jsonplaceholder.typicode.com/todos").then(function(response){
         //console.table(response.data);
         $scope.data=response.data; //data yazmazsan status, method fln geliyor
     })
 
-/*    
+/*
     $scope.data=[
         {"id":1,"body":"İlk Madde"},
         {"id":2,"body":"Yapılacaklardan 2. Madde"},
@@ -21,7 +21,13 @@ aTodo.controller("todocontrol",function($scope,$http){
     $scope.addNewTodo = function(newData){
 
         $scope.data.push(
-            {"id": 10,"body": newData}
+            {
+                "userId": 77,
+                "id": $scope.data.length + 1,
+                "title": newData,
+                "completed": false
+              }
+
         );                                              // datayı json olarak push ettik
         $scope.text = '';                               // inputu boşaltır.
 
